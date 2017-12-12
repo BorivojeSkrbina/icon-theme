06bbbf1050b5dfea7484e95622cf9c5ea2f70708
Borivoje Skrbina <borivoje.s@gmail.com>
2017-12-12 15:37:56 +0100

    team slider implementacija

--- js/main.js
+++ js/main.js
@@ -21,35 +21,63 @@ $(document).ready(function () {
 
     //CIRCLE PROJECT
     $(window).scroll(function () {
+        percent();
+    });
+    percent();
+
+
+    function percent() {
         var vh = $(window).height();
         var scroll = $(window).scrollTop();
 
         $('.cont').each(function () {
             var position = $(this).offset().top;
-            
-            if(position < vh + scroll - 100){
-                var val = parseInt($(this).attr('data-pct'));
-            var $circle = $(this).find('.svg .bar');
 
-             
+            if (position < vh + scroll - 100) {
+                var val = parseInt($(this).attr('data-pct'));
+                var $circle = $(this).find('.svg .bar');
+
+
                 var r = $circle.attr('r');
                 var c = Math.PI * (r * 2);
 
-               
+
 
                 var pct = ((100 - val) / 100) * c;
 
                 $circle.css({strokeDashoffset: pct});
             }
-            
-            
 
-                //$('#cont').attr('data-pct',val);
-            
+
+
+            //$('#cont').attr('data-pct',val);
+
         });
-    });
+    }
 
+    if ($('.owl-carousel').length > 0) {
+        $('.team').owlCarousel({
+            loop: true,
+            margin: 10,
+            dots: false,
+            nav: true,
+            navText: ['<', '>'],
+            responsiveClass: true,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                480: {
+                    items: 2
+                },
+                768: {
+                    items: 3
+                }
+            }
+        });
+        
 
+    }
 
 
 });
